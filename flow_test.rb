7c3dfb1d40ad6c1270a4e8ff25bd75a2b667f0ed
Harry Gueguen <harry.gueguen@gmail.com>
2018-03-20 08:42:04 +0100

    should be working

--- flow_test.rb
+++ flow_test.rb
@@ -2,7 +2,6 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-
 def send_msg(access_token, recipient, message)
 	uri = URI.parse("https://graph.facebook.com/v2.6/me/messages?access_token=#{access_token}")
 	request = Net::HTTP::Post.new(uri)
@@ -29,12 +28,24 @@ def send_msg(access_token, recipient, message)
 	puts response.body
 end
 
-
-# Etape 1 : Definir intervalles de minutes
-# Etape 2 : Mettre les intervalles dans un tableau
-# Etape 3 : Iterer dans le tableau
+# ===> 1 minute = 60 secondes _ 8 msgs sur 14h _ 14/8 = 1,75 _ Interval aleatoire entre 1h et 2,5 h
+# ===> 1h = 3600 secondes, 2h30 = 9000 secondes
+# ===> 14h = 50 400 secs
 
 access_token = "EAAIdD3Re5ZC8BABMdF8ZB51w4n8QZCEVWHOLZB7t5RwZCY5MtelXzZAzhLZAAmNKucuTleMPKZCBttNmcqqVFQOIuFrunL5oyzDpceealPh7HvN52GSabKRm2QShKnVZBSsZAURvZCtX4qndU7kgPytoZAkkZBxjetawtnNZBkmYZBCZCpb26QZDZD"
 recipient = "1805001089562157"
 
-send_msg(access_token, recipient, "Hey")
+tableau_intervalles = []
+total = 0
+begin
+	nouveau_temps = (3600 + rand(5400))
+	total = total + nouveau_temps
+	tableau_intervalles << nouveau_temps
+until total > 50400
+
+tableau_intervalles.each do |i|
+	sleep(i)
+	send_msg(access_token, recipient, "Dsl de te deranger ! Juste 3 questions : Tu fais quoi ? Comment tu te sens ? Es tu dans un etat de flow ?")
+end
+
+send_msg(access_token, recipient, "C'est la fin de la journée ! N'oublie pas de me relancer demain matin")
